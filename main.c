860d9df906b9f4e48baa5ded6af1bbad79f13d5d
tuyenlt <keplerlu@gmail.com>
2023-03-01 21:14:45 +0700

    bai xam lon

--- main.c
+++ main.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int compare(const void* a, const void* b)
+{
+    const int* x = (int*) a;
+    const int* y = (int*) b;
+    if (*x > *y)
+        return 1;
+    else if (*x < *y)
+        return -1;
+    return 0;
+}
 
 int main()
 {
-    int count[100001] = {0};
-    int arr[100];
+    int a[100];
     int n;
     scanf("%d",&n);
-    int totals = 0;
     for(int i=0; i<n; i++){
-        scanf("%d",&arr[i]);
-        count[arr[i]]++;
-        if(count[arr[i]] > 1) totals++;
+        scanf("%d",&a[i]);
     }
-    printf("%d\n",totals);
-    for(int i=0;i<n;i++){
-        if(count[arr[i]] > 1){
-            printf("%d ",arr[i]);
-            count[arr[i]] = 0;
+    qsort(a,n,sizeof(int),compare);
+    int lp = 2;
+    while(lp--){
+        for(int i=0;i<n;i++){
+            if(a[i] % 2 == (1-lp))printf("%d ",a[i]);
         }
     }
     return 0;
