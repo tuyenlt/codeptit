8aa4cf7fe6ad8d0560d881e6ca6a6783fcd7b1ee
tuyenlt <keplerlu@gmail.com>
2023-03-03 18:48:53 +0700

    dit me

--- main.c
+++ main.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 #include <stdlib.h>
 
-int compare(const void* a, const void* b)
-{
-    const int* x = (int*) a;
-    const int* y = (int*) b;
-    if (*x > *y)
-        return 1;
-    else if (*x < *y)
-        return -1;
+int is_fi(long long *fi,long long n){
+    for(int i=0;i<85;i++){
+        if(n == fi[i]){
+            return 1;
+        }else if(n < fi[i]) break;
+    }
     return 0;
 }
 
 int main()
 {
-    int a[100];
-    int n;
-    scanf("%d",&n);
-    for(int i=0; i<n; i++){
-        scanf("%d",&a[i]);
+    int test;
+    scanf("%d",&test);
+    long long fi[86];
+    fi[0] = 0;
+    fi[1] = 1;
+    for(long long i=2;i<86;i++){
+        fi[i] = fi[i-1] + fi[i-2];
     }
-    qsort(a,n,sizeof(int),compare);
-    int lp = 2;
-    while(lp--){
-        for(int i=0;i<n;i++){
-            if(a[i] % 2 == (1-lp))printf("%d ",a[i]);
-        }
+
+    while(test--){
+        long long n;
+        scanf("%lld",&n);
+        if(is_fi(fi,n))printf("YES\n");
+        else printf("NO\n");
+
     }
-    return 0;
+    return 0;    
 }
