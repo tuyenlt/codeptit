c49e8f8140ce3d0f46199e3d72706bfc8acaf4d2
tuyenlt <keplerlu@gmail.com>
2023-05-12 11:08:49 +0700

    update

---
+++ Sample/build/CMakeFiles/Sample.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/e/TuyenLt/LinuxWorkSpace/C/codeptit/Sample/main.c" "CMakeFiles/Sample.dir/main.c.o" "gcc" "CMakeFiles/Sample.dir/main.c.o.d"
+  "/mnt/e/TuyenLt/LinuxWorkSpace/C/codeptit/Sample/testlib.c" "CMakeFiles/Sample.dir/testlib.c.o" "gcc" "CMakeFiles/Sample.dir/testlib.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Sample/build/CMakeFiles/Sample.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sample.dir/main.c.o"
+  "CMakeFiles/Sample.dir/main.c.o.d"
+  "CMakeFiles/Sample.dir/testlib.c.o"
+  "CMakeFiles/Sample.dir/testlib.c.o.d"
+  "Sample"
+  "Sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
